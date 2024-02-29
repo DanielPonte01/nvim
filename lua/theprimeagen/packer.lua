4c155dcf0421969d86b7135013e0d8082bd6e91b
Daniel Ponte <contato@danielponte.com.br>
2024-02-29 11:21:19 -0300

    initial commit

---
+++ lua/theprimeagen/packer.lua
@@ -0,0 +1,120 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+-- Only required if you have packer configured as `opt`
+vim.cmd.packadd('packer.nvim')
+
+return require('packer').startup(function(use)
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
+
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.4',
+        -- or                            , branch = '0.1.x',
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }
+
+    use("nvim-tree/nvim-web-devicons")
+
+    use({
+        'rose-pine/neovim',
+        as = 'rose-pine',
+        config = function()
+            vim.cmd('colorscheme rose-pine')
+        end
+    })
+    use { "catppuccin/nvim", as = "catppuccin" }
+
+    use({
+        "folke/trouble.nvim",
+        config = function()
+            require("trouble").setup {
+                icons = false,
+                -- your configuration comes here
+                -- or leave it empty to use the default settings
+                -- refer to the configuration section below
+            }
+        end
+    })
+
+    use {
+        'nvim-treesitter/nvim-treesitter',
+        run = function()
+            local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+            ts_update()
+        end,}
+
+
+        use {
+            'vimwiki/vimwiki',
+            config = function()
+                vim.g.vimwiki_list = {
+                    {
+                        path = '/home/daniel/Documents/mywiki',
+                        syntax = 'markdown',
+                        ext = '.md',
+                    }
+                }
+            end
+        }
+
+       use {
+        'windwp/nvim-autopairs',
+        opts = {
+          fast_wrap = {},
+          disable_filetype = { "TelescopePrompt", "vim" },
+        },
+        config = function(_, opts)
+          require("nvim-autopairs").setup(opts)
+
+          -- setup cmp for autopairs
+          local cmp_autopairs = require "nvim-autopairs.completion.cmp"
+          require("cmp").event:on("confirm_done", cmp_autopairs.on_confirm_done())
+        end,
+      }
+
+
+        use("nvim-treesitter/playground")
+        use("theprimeagen/harpoon")
+        use("theprimeagen/refactoring.nvim")
+        use("mbbill/undotree")
+        use("tpope/vim-fugitive")
+        use("ap/vim-css-color")
+        use("nvim-treesitter/nvim-treesitter-context");
+        use("ThePrimeagen/vim-be-good")
+
+        use {
+            'VonHeikemen/lsp-zero.nvim',
+            branch = 'v1.x',
+            requires = {
+                -- LSP Support
+                {'neovim/nvim-lspconfig'},
+                {'williamboman/mason.nvim'},
+                {'williamboman/mason-lspconfig.nvim'},
+
+                -- Autocompletion
+                {'hrsh7th/nvim-cmp'},
+                {'hrsh7th/cmp-buffer'},
+                {'hrsh7th/cmp-path'},
+                {'saadparwaiz1/cmp_luasnip'},
+                {'hrsh7th/cmp-nvim-lsp'},
+                {'hrsh7th/cmp-nvim-lua'},
+
+                -- Snippets
+                {'L3MON4D3/LuaSnip'},
+                {'rafamadriz/friendly-snippets'},
+            }
+        }
+
+
+
+
+
+
+        use("folke/zen-mode.nvim")
+        use("github/copilot.vim")
+        use("eandrju/cellular-automaton.nvim")
+        use("laytan/cloak.nvim")
+
+    end)
+
+
